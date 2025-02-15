4a2fa75e8ac41ceda94fd12ffd22dcc6b9bdddd9
Mason S. <10767492@uvu.edu>
2025-02-15 13:41:40 -0700

    Made more animations and created an animation controller w/ script

---
+++ Spring2025CoreGame/Assets/Scripts/AnimateCharacter.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateCharacter : MonoBehaviour
+{
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleAnimation();
+    }
+
+    private void HandleAnimation()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("Jump");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("Fall");
+        }
+
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            animator.SetTrigger("Run");
+        }
+        else { animator.SetTrigger("Idle"); }
+
+
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            animator.SetTrigger("Hit");
+        }
+    }
+}
